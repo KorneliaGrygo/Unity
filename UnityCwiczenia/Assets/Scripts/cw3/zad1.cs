50fc4082e71afbe4e564b0d37a42048e69c0abca
KorneliaGrygo <grygo96@o2.pl>
2021-11-06 20:12:08 +0100

    cw3_zad2

---
+++ UnityCwiczenia/Assets/Scripts/cw3/zad1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zad1 : MonoBehaviour
+{
+    public float speed = 2.0f;
+    Rigidbody rigi;
+    Vector3 pA = new Vector3(0,0,0);
+    Vector3 pB = new Vector3(10,0,0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigi = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(pA, pB, Mathf.PingPong(Time.time, speed));
+    }
+}
