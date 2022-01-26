61c326cee292c2c0fe50e8779f6a6e354e86c955
KorneliaGrygo <grygo96@o2.pl>
2022-01-26 22:28:07 +0100

    Dodane skrypty do prefabow serduszek, zmiany w coliderach playera, zwiekszenie tla i naprawy

---
+++ GoGoDolls/Assets/Scripts/HearthCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HearthCollision : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {   
+            PointsCounter.coins++;
+            Destroy(gameObject, 0.01f);   
+    }
+
+}
---
+++ GoGoDolls/Assets/Scripts/PointsCounter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PointsCounter : MonoBehaviour
+{
+    public TextMeshProUGUI score;
+    public static int coins;
+    void Awake()
+    {
+        score = GetComponent<TextMeshProUGUI>();
+    }
+    void Update()
+    {
+        score.text = coins.ToString();
+    }
+}
