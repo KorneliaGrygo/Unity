fc536a63f42e89feee1a6d025df11a501773f907
KorneliaGrygo <grygo96@o2.pl>
2022-01-26 22:43:13 +0100

    Zmiana szybkosci ruchu playera oraz sily skoku

--- GoGoDolls/Assets/Scripts/PlayerMovement.cs
+++ GoGoDolls/Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller2D;
     float horizontalMove = 0f;
-    public float runSpeed = 40f;
+    public float runSpeed = 20f;
     bool jump = false;
     bool crouch = false;
 
